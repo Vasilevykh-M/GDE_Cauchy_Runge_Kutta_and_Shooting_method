d56536a97cfabeaf65e67c714992d0e8b8d75564
Andrey Zhukov <ryanworld225@yandex.ru>
2021-12-15 14:17:04 +0300

    Переписал свое, погрешности вроде адекватные финальные. Красивый вывод позже замучу, заодно проверю все

--- laba7/main.cpp
+++ laba7/main.cpp
@@ -72,7 +72,7 @@ double tauImp(double h)
 
 double f2(double t, double x)
 {
-	return 0.1 * sin(M_PI * x) * variant + 0.1 * 0.1 * t * variant * pow(M_PI, 2) * sin(M_PI * x);
+	return 0.1 * sin(M_PI * x) * variant + 0.1 * ksi * t * variant * pow(M_PI, 2) * sin(M_PI * x);
 }
 
 double FindH(double h, double alpha, double x, double y, double& z) /*я вроде понимаю как это работает*/
@@ -93,31 +93,6 @@ double FindH(double h, double alpha, double x, double y, double& z) /*я вроде по
 	z = z + (l1 + 2 * l2 + 2 * l3 + l4) / 6.;
 
 	return y;
-}
-
-vector<double> ExplicitShem(int n, int cur_i)
-{
-	vector<double> u_cur(n + 1);
-	vector<double> u_next(n + 1);
-
-	double h = 1. / n;
-	double tau = tauExp(h);
-
-	for (int i = 0; i < u_cur.size(); ++i)
-	{
-		u_cur[i] = h * i;
-	}
-
-	for (int i = 0; i < cur_i; ++i)
-	{
-		for (int j = 1; j < n; ++j)
-		{
-			u_next[j] = u_cur[j] + tau * ksi * (u_cur[j + 1] - 2 * u_cur[j] + u_cur[j - 1]) / pow(h, 2) + tau * f2(tau * i, j * h);
-		}
-		u_cur = u_next;
-	}
-
-	return u_cur;
 }
 
 vector<double> solveSLAU(vector<vector<double>> a, vector<double> b)
@@ -146,94 +121,116 @@ vector<double> solveSLAU(vector<vector<double>> a, vector<double> b)
 	return x;
 }
 
-vector<double> InplicitShem(int n, int cur_i)
-{
-	double h = 1. / n;
-	double tau = tauImp(h);
-	double d = tau * ksi / pow(h, 2);
-
-	vector<vector<double>> a(n - 1,(vector<double>(n-1)));
-
-	for (int i = 0; i < a.size(); ++i)
-	{
-		if (i - 1 >= 0)
-			a[i][i - 1] = -d;
-
-		a[i][i] = 1 + 2 * d;
-
-		if (i + 1 < a.size())
-			a[i][i + 1] = -d;
-	}
-
-	vector<double> u_cur(n + 1);
-	for (int i = 0; i < u_cur.size(); ++i)
-		u_cur[i] = i * h;
-
-	vector<double> b(n - 1);
-
-	for(int i=0;i<cur_i;++i)
-	{
-		for (int j = 0; j < b.size(); ++j)
-			b[j] = u_cur[j + 1] + tau * f2(tau * (i + 1), h * (j + 1));
-
-		b[b.size() - 1] += d;
-		b = solveSLAU(a, b);
-
-		for (int j = 0; j < b.size(); ++j)
-			u_cur[j + 1] = b[j];
-	}
-
-	return u_cur;
-
-}
-
 void FDM()
 {
-	vector<double> cur_x;
-	double Del_T = 0;
 	for(int n=8;n<=32;n*=2)
 	{
+		double Del_T = 0;
 		double h = 1. / n;
+		double tau = tauExp(h);
+		vector<double> cur_x(n+1);
+		vector<double> next_x(n + 1);
+
+		for (int i = 0; i < cur_x.size(); ++i)
+			cur_x[i] = h * i;
+
+		next_x = cur_x;
+
 		for(int i=1;i*tauExp(h)<=1;++i)
 		{
-			cur_x = ExplicitShem(n, i);
+			//cur_x = ExplicitShem(n, i);
 			double delta = 0;
-			double tau = tauExp(h) * i;
 
-
-			for (int j = 0; j <= n; ++j)
+			for (int j = 1; j < cur_x.size()-1; ++j)
 			{
-				double max = 0;
-				if (abs(utx(tau, j * h) - cur_x[i]) > max)
-					max = abs(utx(tau, j * h) - cur_x[i]);
+				next_x[j] = cur_x[j] + tau * ksi * (cur_x[j + 1] - 2 * cur_x[j] + cur_x[j - 1]) /(h*h) + tau * f2(tau*i, j * h);
+			}
+
+			cur_x= next_x;
+
+			double max = 0;
+
+			for (int j = 0; j <cur_x.size()-1; ++j)
+			{
+				if (abs(utx(tau*i, j * h) - cur_x[j]) > max)
+					max = abs(utx(tau*i, j * h) - cur_x[j]);
 				delta = max;
 			}
 
+			for(int i=0;i<cur_x.size();++i)
+			{
+				//cout << fixed << setprecision(5) << "|" << cur_x[i];
+			}
+
+			//cout << endl;
+
 			if (delta > Del_T)
 				Del_T = delta;
+
+			//cout << tau*i << endl;
 		}
+
+		cout << Del_T << endl;
 	}
 
 	for (int n = 8; n <= 32; n *= 2)
 	{
+		double Del_T = 0;
 		double h = 1. / n;
+		double tau = tauImp(h);
+		double d = tau * ksi / pow(h, 2);
+
+		vector<double> cur_x(n + 1);
+		vector<double> next_x(n + 1);
+
+		for (int i = 0; i < cur_x.size(); ++i)
+			cur_x[i] = h * i;
+
+		next_x = cur_x;
+
+		vector<vector<double>> a(n - 1, (vector<double>(n - 1)));
+
+		for (int i = 0; i < a.size(); ++i)
+		{
+			if (i - 1 >= 0)
+				a[i][i - 1] = -d;
+
+			a[i][i] = 1 + 2 * d;
+
+			if (i + 1 < a.size())
+				a[i][i + 1] = -d;
+		}
+
 		for (int i = 1; i * tauImp(h) <= 1; ++i)
 		{
-			cur_x = InplicitShem(n, i);
-			double delta = 0;
-			double tau = tauImp(h) * i;
 
-			for (int j = 0; j <= n; ++j)
+			vector<double> b(n - 1);
+
+			for (int j = 0; j < b.size(); ++j)
+				b[j] = cur_x[j + 1] + tau * f2(tau * (i+1), h * (j+1) );
+
+			b[b.size() - 1] += d;
+			b = solveSLAU(a, b);
+
+			for (int j = 0; j < b.size(); ++j)
+				cur_x[j + 1] = b[j];
+			
+
+			double delta = 0;
+
+			for (int j = 0; j <cur_x.size()-1; ++j)
 			{
 				double max = 0;
-				if (abs(utx(tau, j * h) - cur_x[i]) > max)
-					max = abs(utx(tau, j * h) - cur_x[i]);
+				if (abs(utx(tau*i, j * h) - cur_x[j]) > max)
+					max = abs(utx(tau*i, j * h) - cur_x[j]);
 				delta = max;
 			}
 
 			if (delta > Del_T)
 				Del_T = delta;
 		}
+
+		cout << Del_T << endl;
 	}
 }
 
@@ -329,5 +326,6 @@ void shooting_metod()
 
 void main()
 {
-	shooting_metod();
+	//shooting_metod();
+	FDM();
 }
